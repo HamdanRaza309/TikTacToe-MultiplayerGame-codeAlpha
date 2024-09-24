a3a936e3176e1d4a2c3936ef2c06970a7c48c83d
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-09-24 10:00:57 +0500

    Complete basic design phase.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './App';
+import React, { useState } from 'react';
+import './App.css'; // Ensure this points to your CSS file
+import Square from './Square/Square';
+
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  const [gameState, setGameState] = useState(renderFrom);
+
+  return (
+    <div className='main-div'>
+      <div className="move-detection">
+        <div className='left-side'>You</div>
+        <div className='right-side'>Opponent</div>
+      </div>
+      <div>
+        <h1 className='game-heading water-bg'>Tic Tac Toe</h1>
+        <div className="square-wrapper">
+          {gameState.map((arr, rowIndex) => (
+            <div key={rowIndex}>
+              {arr.map(e => (
+                <Square key={e} />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
